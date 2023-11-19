c04d76dc64bd0950b988bff6198146efc333bb99
Md Abdur Sobhan Riad <93583569+riad5089@users.noreply.github.com>
2023-11-19 16:24:45 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from app import get_qa_chain, create_vector_db
 
-st.title("Codebasics Q&A 🌱")
+st.title("Question an Answering System 🌱")
 # btn = st.button("Create Knowledgebase")
 # if btn:
 #     create_vector_db()
